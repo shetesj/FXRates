f87176ccedd6c79812a25836e60bce58f7c25b85
Shailendra Shete <shetes@gmail.com>
2016-04-01 18:05:14 -0400

    Init

---
+++ FXRates/FXMain.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FXMain.aspx.cs" Inherits="FXRate.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePageMethods="true">
+        </asp:ScriptManager>
+        <div class="container">
+            <asp:Table runat="server" ID="tblMain">
+                <asp:TableRow VerticalAlign="Top">
+                    <asp:TableCell>
+                        <asp:DropDownList ID="ddSource" runat="server">
+                        </asp:DropDownList>
+                    </asp:TableCell>
+                    <asp:TableCell>
+
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:ListBox EnableViewState="true" ID="lstCurrencies" runat="server" SelectionMode="Multiple" Height="200px"></asp:ListBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="3" HorizontalAlign="Center">
+                        <asp:Button ID="btnGetFXRates" runat="server" Text="Button" OnClick="btnGetFXRates_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+            <hr />
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <asp:GridView ID="grdResults" AutoGenerateColumns="false" runat="server" BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="2px" CellPadding="6" CellSpacing="6" ForeColor="Black" GridLines="Vertical">
+                                <AlternatingRowStyle BackColor="White" />
+                                <FooterStyle BackColor="#CCCC99" />
+                                <HeaderStyle HorizontalAlign="Center" BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+                                <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+                                <RowStyle BackColor="#F7F7DE" />
+                                <Columns>
+                                    <asp:BoundField DataField="Key" HeaderText="Target Currency   " />
+                                    <asp:BoundField DataField="Value" HeaderText="Rate" ItemStyle-HorizontalAlign="Right" />
+                                </Columns>
+                            </asp:GridView>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </form>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="http://code.jquery.com/jquery-1.12.2.min.js" integrity="sha256-lZFHibXzMHo3GGeehn1hudTAP3Sc0uKXBXAzHX1sjtk=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        //Called this method on any button click  event for Testing
+        function processJSON(Param1) {
+            PageMethods.ParseJSON(Param1);
+            //$.ajax({
+            //    type: "POST",
+            //    url: "FXMain.aspx/ProcessJSON",
+            //    data: "{ Param1: '" + Param1 + "'}",
+            //    contentType: "application/json; charset=utf-8",
+            //    dataType: "json",
+            //    async: "true",
+            //    cache: "false"
+            //});
+            console.write('In callback function');
+        }
+    </script>
+
+</body>
+</html>
---
+++ FXRates/FXMain.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace FXRate
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string baseURL = "http://apilayer.net/api";
+        private string accessKey = "?access_key=3aa1675d116d7828ce8df0926e4fe95c";
+        private string jsonFormat = "&format=1";
+        private string callback = "&callback=processJSON";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Get currency list
+                var url = baseURL + "/list" + accessKey;
+                var currList = getJSONdata<currList>(url);
+                var datasource = (from x in currList.Currencies
+                                 select new
+                                 {
+                                     x.Key,
+                                     x.Value,
+                                     DisplayText = String.Format("{0} - {1}", x.Key, x.Value)
+                                 });
+
+                ddSource.DataSource = datasource;
+                ddSource.DataTextField = "DisplayText";
+                ddSource.DataValueField = "Key";
+                ddSource.DataBind();
+                
+                lstCurrencies.DataSource = datasource;
+                lstCurrencies.DataTextField = "DisplayText";
+                lstCurrencies.DataValueField = "Key";
+                lstCurrencies.DataBind();
+            }
+        }
+
+        public class currList
+        {
+            public string Success { get; set; }
+            public string Terms { get; set; }
+            public string Privacy { get; set; }
+            public Dictionary<string, string> Currencies { get; set; }
+        }
+
+        public class CurrencyRates
+        {
+            public string Success { get; set; }
+            public string Terms { get; set; }
+            public string Privacy { get; set; }
+            public string License { get; set; }
+            public int TimeStamp { get; set; }
+            public string Source { get; set; }
+            public Dictionary<string, decimal> Quotes { get; set; }
+        }
+
+        public class OXR
+        {
+            public string Disclaimer { get; set; }
+            public string License { get; set; }
+            public int TimeStamp { get; set; }
+            public string Base { get; set; }
+            public Dictionary<string, decimal> Rates { get; set; }
+        }
+
+        private static T getJSONdata<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+
+        protected void btnGetFXRates_Click(object sender, EventArgs e)
+        {
+            string source = ddSource.SelectedValue;
+            StringBuilder targets = new StringBuilder();
+            string targetList = "";
+
+            foreach (ListItem li in lstCurrencies.Items)
+            {
+                if (li.Selected)
+                    targets.Append(li.Value + ",");
+            }
+
+            if (targets.Length > 0)
+            {
+                targetList = targets.ToString().Substring(0, targets.Length - 1);
+            }
+
+            // ------- Should use this but free API does not allow changing source
+            //string url = baseURL + "/live" + accessKey + "&source=" + source
+            //    + "&currencies=" + targetList + jsonFormat;
+            
+            // Hence using this where I am fixing source to USD
+            string url = baseURL + "/live" + accessKey + "&source=USD"
+                + "&currencies=" + targetList + jsonFormat;
+
+            getRatesData(url);
+            
+        }
+
+        public void getRatesData(string url)
+        {
+            var CurrencyRates = getJSONdata<CurrencyRates>(url);
+            grdResults.DataSource = CurrencyRates.Quotes;
+            grdResults.DataBind();
+        }
+    }
+}
